1ffdb2eb16d78e6b3e920d6ba340025a0f1f9224
dniloo13 <d.niloo@gmail.com>
2025-06-15 20:47:23 -0300

    Ajuste na msg Final

--- src/main/java/br/com/dml/controllers/GreetingController.java
+++ src/main/java/br/com/dml/controllers/GreetingController.java
@@ -11,7 +11,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "First  - Hello , %s!";
 
     private final AtomicLong counter = new AtomicLong();
     // http://localhost:8080/greeting?name=Danilo
